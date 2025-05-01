04a871d51064bf8f952c3ec18000c473a67c19bf
renehauck <surfer.re@gmail.com>
2025-05-01 12:05:09 +0200

    fix: update publish directory to ./dist in deploy workflow feat: set base URL for Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: 'https://coasoft.github.io/trezl-public',
   vite: {
     plugins: [tailwindcss()]
   }
